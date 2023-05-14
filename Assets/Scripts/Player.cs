14b15be424c28ada013e68409786a446191ba30d
Yigit Akkaya <ygtakky@gmail.com>
2023-05-14 01:13:18 -0400

    Added Player walking animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
     
+    private bool isWalking = false;
+    
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -30,8 +32,14 @@ public class Player : MonoBehaviour
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * (moveSpeed * Time.deltaTime);
-        
+
+        isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+    
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
