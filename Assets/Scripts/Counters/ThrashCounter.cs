f06d23b1fa1c96d81f08163123e8b5dc06f29ed7
Yigit Akkaya <ygtakky@gmail.com>
2023-05-19 02:02:32 -0400

    Added Thrash Counter

---
+++ Assets/Scripts/Counters/ThrashCounter.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ThrashCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            player.GetKitchenObject().DestroySelf();
+        }
+    }
+
+}
