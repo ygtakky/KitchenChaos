5bf3dc1315654560842a7b68c42ad3617f328730
Yigit Akkaya <ygtakky@gmail.com>
2023-05-23 22:29:43 -0400

    Added Delivery Counter and Custom Shader

---
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+            {
+                // Only accepts plates
+                player.GetKitchenObject().DestroySelf();
+            }
+        }
+    }
+}
