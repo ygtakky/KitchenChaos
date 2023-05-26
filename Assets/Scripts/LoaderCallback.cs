cfd6db40e35230a6b5f81d6831fe299038fb4e7a
Yigit Akkaya <ygtakky@gmail.com>
2023-05-26 00:17:53 -0400

    Added MainMenuUI and Loading scene

---
+++ Assets/Scripts/LoaderCallback.cs
@@ -0,0 +1,17 @@
+using System;
+using UnityEngine;
+
+public class LoaderCallback : MonoBehaviour
+{
+    private bool isFirstUpdate = true;
+
+    private void Update()
+    {
+        if (isFirstUpdate)
+        {
+            isFirstUpdate = false;
+            
+            Loader.LoaderCallback();
+        }
+    }
+}
