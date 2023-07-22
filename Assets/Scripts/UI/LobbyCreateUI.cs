cc244a3d484728963d18526d60f30380d3dba450
Yigit Akkaya <97368031+ygtakky@users.noreply.github.com>
2023-07-21 23:09:32 -0400

    Added Gamepad Support to UI

--- Assets/Scripts/UI/LobbyCreateUI.cs
+++ Assets/Scripts/UI/LobbyCreateUI.cs
@@ -33,6 +33,8 @@ public class LobbyCreateUI : MonoBehaviour
     public void Show()
     {
         gameObject.SetActive(true);
+        
+        createPublicButton.Select();
     }
     
     private void Hide()
